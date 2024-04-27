524f90a95d58af917a0e3d133278d86fa6b3eecf
cheez <cheezballz03@gmail.com>
2024-04-27 11:29:18 +0300

    Add Serilog logging functionality
    Added logging functionality using Serilog to the OptiCompare project. Introduced a new CustomLogger class and injected it into the PhoneService class, logging the method calls within this service. Modified the .csproj file to include necessary Serilog package references.

--- Services/PhoneService.cs
+++ Services/PhoneService.cs
@@ -1,19 +1,25 @@
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
 using OptiCompare.DTOs;
 using OptiCompare.Extensions;
 using OptiCompare.Mappers;
 using OptiCompare.Models;
 using OptiCompare.Repositories;
+using Serilog;
+using Serilog.Core;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
 using X.PagedList;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace OptiCompare.Services;
 
 public class PhoneService : IPhoneService
 {
     private readonly IRepository<Phone> _phoneRepository;
-
+    private readonly CustomLogger _customLogger = new("Logs/PhoneServiceLogs/log.json");
     public PhoneService(IRepository<Phone> phoneRepository)
     {
         _phoneRepository = phoneRepository;
@@ -21,7 +27,8 @@ public class PhoneService : IPhoneService
 
     #region IRepository members
     public async Task<IPagedList<PhoneDto>> GetPaginatedPhones(int? page, string? searchString)
-    {
+    {   
+        _customLogger.Log($"GetPaginatedPhones(int? {page}, string? {searchString}); was called.");
         const int pageSize = 7;
         var pageNumber = page ?? 1;
 
@@ -53,12 +60,14 @@ public class PhoneService : IPhoneService
 
     public async Task<bool> AddPhone(PhoneDto phoneDto)
     {
+        _customLogger.Log($"AddPhone(PhoneDto {phoneDto.modelName}); was called.");
         bool result = await _phoneRepository.Add(phoneDto.ToPhone());
         return result;
     }
 
     public async Task<bool> EditPhone(int? id, PhoneDto phoneDto)
     {
+        _customLogger.Log($"EditPhone(int? {id}, PhoneDto? {phoneDto.modelName}); was called.");
         if (id == null)
         {
             throw new Exception("Redundant phone id!");
@@ -75,6 +84,7 @@ public class PhoneService : IPhoneService
 
     public async Task<bool> DeletePhone(int id)
     {
+        _customLogger.Log($"DeletePhone(int? {id}); was called.");
         var phone = await _phoneRepository.Get(id);
         bool result = await _phoneRepository.Remove(phone);
         return result;
